e19cff0d1ba18fc08b50771272550db4ab37fd26
Satish Yadav <monstersatish@live.com>
2018-06-10 00:35:55 +0530

    Sql added

---
+++ Links/Sql/Links.sql
@@ -0,0 +1,22 @@
+﻿CREATE DATABASE Links;
+GO
+
+USE [Links];
+GO
+
+/****** Object: Table [dbo].[Link] Script Date: 10-06-2018 00:33:10 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Link] (
+    [Id]           INT            IDENTITY (1, 1) NOT NULL,
+    [OriginalLink] NVARCHAR (MAX) NULL,
+    [CreatedBy]    NVARCHAR (200) NOT NULL,
+    [CreatedOn]    DATETIME2 (7)  NOT NULL,
+    [Stats]        NVARCHAR (MAX) NULL
+);
+
+GO
